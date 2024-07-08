89da3240ab10440c9d80c103826178559897923b
Saad Salmany <mohdsaad88842@gmail.com>
2024-07-08 17:56:32 +0530

    diabled locomotive

--- app/page.js
+++ app/page.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import LocomotiveWrapper from './Components/LocomotiveWrapper'
 
 function page() {
   return (
     <>
-    <LocomotiveWrapper>
       <div className='text-primary-red w-full h-screen bg-secondary-blue ' >Hello Saad</div>
       <div className='text-primary-red w-full h-screen bg-primary-red ' >Hello Saad</div>
-    </LocomotiveWrapper>
     </>
   )
 }
