dbf31d4e6214ce28d09a5158fe0894eb3c4025c0
Saad Salmany <mohdsaad88842@gmail.com>
2024-07-08 17:54:44 +0530

    Integrated Locomotive

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import LocomotiveWrapper from './Components/LocomotiveWrapper'
 
 function page() {
   return (
-    <div className='text-primary-red' >Hello Saad</div>
+    <>
+    <LocomotiveWrapper>
+      <div className='text-primary-red w-full h-screen bg-secondary-blue ' >Hello Saad</div>
+      <div className='text-primary-red w-full h-screen bg-primary-red ' >Hello Saad</div>
+    </LocomotiveWrapper>
+    </>
   )
 }
 
