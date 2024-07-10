08c961e253b278a977b4a9a9112d3bb96a66fd24
Saad Salmany <mohdsaad88842@gmail.com>
2024-07-10 14:24:24 +0530

    added locomotive again for testing if it works

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import LocomotiveWrapper from "./Components/LocomotiveWrapper";
 
 function page() {
   return (
     <main className="w-full h-screen">
+      <LocomotiveWrapper>
+
       <Navbar />
+      <div className="w-full h-screen bg-secondary-blue"></div>
+      <div className="w-full h-screen bg-primary-red"></div>
+      </LocomotiveWrapper>
     </main>
   );
 }
