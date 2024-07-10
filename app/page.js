92502c6d16d04fc41b0d904e93f250c6766fbf96
Saad Salmany <mohdsaad88842@gmail.com>
2024-07-10 14:28:33 +0530

    removed locomotive

--- app/page.js
+++ app/page.js
@@ -5,12 +5,10 @@ import LocomotiveWrapper from "./Components/LocomotiveWrapper";
 function page() {
   return (
     <main className="w-full h-screen">
-      <LocomotiveWrapper>
 
       <Navbar />
       <div className="w-full h-screen bg-secondary-blue"></div>
       <div className="w-full h-screen bg-primary-red"></div>
-      </LocomotiveWrapper>
     </main>
   );
 }
