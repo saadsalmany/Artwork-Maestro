f76ed65dfc59588ab79860e7c64ab3de8aa6ac02
Saad Salmany <mohdsaad88842@gmail.com>
2024-07-11 12:59:54 +0530

    navbar improved

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-white border-b-[2px] ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20 items-center">
           {/* Desktop menu */}
