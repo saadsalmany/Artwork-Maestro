181aa2b4a5d89517f209b080b23f30752539ef4c
Saad Salmany <mohdsaad88842@gmail.com>
2024-08-15 20:36:01 +0530

    again enabled speed insight correctly

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
-import { injectSpeedInsights } from "@vercel/speed-insights/*";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -57,7 +57,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Analytics />
-        <injectSpeedInsights/>
+        <SpeedInsights/>
       </body>
     </html>
   );
