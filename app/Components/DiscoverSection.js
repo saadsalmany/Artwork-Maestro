12705298a7e9a2d103213c1ba6a6f9ac0f3f7e58
Saad Salmany <mohdsaad88842@gmail.com>
2024-08-23 21:20:11 +0530

    .

--- app/Components/DiscoverSection.js
+++ app/Components/DiscoverSection.js
@@ -33,7 +33,7 @@ const ProductRangeCard = () => {
           <div className="tablet:w-1/2 section">
             <div className="relative w-80 desktop:w-full">
               <Image
-                src="/discover.png"
+                src="/Discover.png"
                 alt="Handcrafted hornware collection"
                 width={500}
                 height={0}
