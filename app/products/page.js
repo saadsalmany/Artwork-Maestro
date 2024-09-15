567d9c865ac877341c6fe249921ca96ec1501520
Saad Salmany <mohdsaad88842@gmail.com>
2024-09-15 10:42:24 +0530

    fixed sitemap

--- app/products/page.js
+++ app/products/page.js
@@ -15,7 +15,7 @@ function ProductPage() {
           content="Explore exclusive art collections featuring unique artwork from emerging artists. Discover your next masterpiece and support talented creators!"
         />
         <link rel="canonical" href="https://www.artworkmaestro.com/products" />
-        <meta name="keywords" content="artwork maestro, Picture Frames, Boxes, Home decor, hornware, art collections, exclusive artwork, emerging artists, contemporary art, art gallery" />
+        <meta name="keywords" content="artwork maestro, Picture Frames, Boxes, decorative-boxes, handicraft boxes, Home decor, hornware, art collections, exclusive artwork, emerging artists, contemporary art, art gallery" />
         <meta name="robots" content="index, follow" />
       </Head>
       <div className="bg-zinc-100">
